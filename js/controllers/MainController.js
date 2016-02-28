b3ba09c3ecd21544629ae464b0b741043594441b
Maria <mmorrison1978@gmail.com>
2016-02-27 19:17:11 -0700

    table added

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,11 @@
-app.controller('MainController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService){
+app.controller('MainController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService) {
+  categoryArray = ["dark", "cold", "dry", "hot", "awesome", "summer", "warm", "winter", "lucid", "spring"].sort();
   var promise = MyService.gotData();
 
   promise.then(function(data) {
-    $scope.teas = data.data
-  })
+    $scope.teas = data.data;
+    console.log($scope.teas);
+  });
+
+
 }])
--- js/services/MyService.js
+++ js/services/MyService.js
@@ -1,11 +1,26 @@
 app.service('MyService', ['$http', '$q', function($http, $q) {
   var deferred = $q.defer();
 
-  $http.get("stock.json").then(function(data) {
+
+
+  $http.get("resources/stock.json").then(function(data) {
     deferred.resolve(data)
   });
 
   this.gotData = function() {
     return deferred.promise
-  }
+  };
+  // 
+  // this.resolvePromise = function() {
+  //   var promise = this.gotData()
+  //   promise.then(function(data) {
+  //     var data = data.data;
+  //   })
+  //   }
+
+
+
+
+
+
 }]);
