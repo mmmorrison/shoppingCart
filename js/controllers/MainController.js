44d39d59326ceed1f3a1c148ffa866e187a101c5
Maria <mmorrison1978@gmail.com>
2016-02-29 15:57:00 -0700

    search works

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,19 @@
 app.controller('MainController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService) {
   var promise = MyService.gotData();
   $scope.x = [];
-  
+
   promise.then(function(data) {
     $scope.teas = data.data;
     for (var i = 0; i < data.data.length; i++) {
       data.data[i].categories.forEach(function(element) {
         $scope.x.push(element);
-
     })
-}
+  }
 
-    $scope.x = _.uniq($scope.x, function(element) {
+  $scope.x = _.uniq($scope.x, function(element) {
       return element
-    });
-  $scope.selected = $scope.x[0];
+  });
+    $scope.selected = $scope.x[0];
   })
 
 
