c01d394ed1ccd6b1d980ec33cdde856ee9b46427
Maria <mmorrison1978@gmail.com>
2016-02-29 06:45:02 -0700

    added services for cart

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,11 +1,15 @@
 app.controller('MainController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService) {
-  categoryArray = ["dark", "cold", "dry", "hot", "awesome", "summer", "warm", "winter", "lucid", "spring"].sort();
+  $scope.categoryArray = ["dark", "cold", "dry", "hot", "awesome", "summer", "warm", "winter", "lucid", "spring"].sort();
   var promise = MyService.gotData();
 
   promise.then(function(data) {
     $scope.teas = data.data;
-    console.log($scope.teas);
   });
+  // 
+  // function CartForm($scope) {
+  //   $scope.invoice = {
+  //       items: [{qty: 0, cost: 0}]
+  //   };
 
 
 }])
--- js/services/MyService.js
+++ js/services/MyService.js
@@ -9,6 +9,33 @@ app.service('MyService', ['$http', '$q', function($http, $q) {
     return deferred.promise
   };
 
+  this.bag = function() {
+    {invoice = {
+      items: [{
+          qty: 0,
+          cost: 0}]
+        };
+  }()
+
+  this.addItem = function() {
+    invoice.items.push({
+          qty: 0,
+          cost: 0
+      });
+  };
+
+  this.removeItem = function(item) {
+      invoice.items.splice(index, 1);
+  },
+
+  this.total = function() {
+    var total = 0;
+    forEach(invoice.items, function(item) {
+      total += item.qty * item.cost;
+      })
+      return total;
+  }
+
 
 
 
