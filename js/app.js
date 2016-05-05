153593118456900365facddbd0e3092588623d41
Maria <mmorrison1978@gmail.com>
2016-05-05 12:40:47 -0600

    trying to fix app.js

--- js/app.js
+++ js/app.js
@@ -8,8 +8,26 @@ app.config(function($locationProvider, $routeProvider){
       templateUrl: 'shoppingCart/partials/cart.html',
       controller: 'CartController'
     })
-    .when('/', {
+    .when('/shoppingCart', {
       templateUrl: 'shoppingCart/partials/tea.html',
       controller: 'MainController'
     })
 });
+
+// app.config(function($routeProvider, $locationProvider) {
+//   $locationProvider.html5Mode(true);
+//     $routeProvider
+//       .when('/resources', {
+//         templateUrl: 'ResourceApp/partials/resources/index.html',
+//         controller: 'ResourcesController'
+//       })
+//       .when('/newresource', {
+//         templateUrl: 'ResourceApp/partials/resources/new.html',
+//         controller: 'NewController'
+//       })
+//       .when('/resource/:id', {
+//         templateUrl: 'ResourceApp/partials/resources/show.html',
+//         controller: 'ShowController'
+//       })
+//
+// });
