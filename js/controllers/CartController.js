c35402292725509c123e0e0bb0f441a6c0210bfa
Maria <mmorrison1978@gmail.com>
2016-05-05 12:11:18 -0600

    prepping for deployment

--- js/controllers/CartController.js
+++ js/controllers/CartController.js
@@ -1,11 +1,5 @@
 app.controller('CartController', ['$scope', '$routeParams', 'MyService', function($scope, $routeParams, MyService){
-  var promise = MyService.gotData();
-  $scope.x = [];
-  $scope.cart = {};
 
-  promise.then(function(data) {
-    $scope.item = data.data;
-  });
 
 
 
